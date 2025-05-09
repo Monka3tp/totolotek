0123ca7a2cf2cab0b5409cc5c6099ec1a0529764
nazwa uzytkownika <143868818+KremikEmik@users.noreply.github.com>
2025-05-09 11:20:29 +0200

    kod

---
+++ main.py
@@ -0,0 +1,35 @@
+import sys
+from random import random, Random, randint
+
+from PyQt6.QtWidgets import QDialog, QApplication, QMessageBox
+
+from layout import Ui_Dialog
+
+
+class MyForm(QDialog):
+    def __init__(self):
+        super().__init__()
+        self.ui = Ui_Dialog()
+        self.ui.setupUi(self)
+
+        #self.ui.generujButton.clicked.connect(self.generuj)
+        self.ui.losujButton.clicked.connect(self.losuj)
+        self.show()
+
+    def losuj(self):
+        obstawione_liczby = self.ui.obstawiamEdit.text()
+        if len(obstawione_liczby) != 6:
+            informacja = QMessageBox()
+            informacja.setText("Wprowadz poprawne liczby!")
+            informacja.exec()
+
+        lista = []
+        while len(lista) < 6:
+            wynik_losowania = randint(1, 49)
+            lista.append(wynik_losowania)
+        self.ui.losowanieEdit.setText(str(lista))
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = MyForm()
+    sys.exit(app.exec())
